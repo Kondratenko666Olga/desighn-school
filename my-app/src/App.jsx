183a1437aaed215bea2141a45f25319a977c9888
Olga Kondratenko <kondratenko666olga@gmail.com>
2025-09-04 17:04:42 +0300

    base styles settings

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import About from './pages/About/About';
