af5227d78a7d6ffc7146c8c48c0d12841ae4a09b
Olga Kondratenko <kondratenko666olga@gmail.com>
2025-09-08 12:00:09 +0300

    footer mobile ready

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import LazyImage from '../../assets/hooks/LazyImage';
 import logo from '/img/logo.svg';
 import './Footer.scss';
+import { footerSocial, footerLinks } from './footerdata';
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="footer">
       <div className="container">
@@ -14,13 +16,41 @@ export default function Footer() {
                 alt="company logo"
                 className="footer-logo"
               />
-              <p className="footer-main__info-text">
+              <p className="footer-main__info-text text">
                 Top learning experiences that create more talent in the world.
               </p>
             </div>
-            <div className="footer-main__links"></div>
+            <div className="footer-main__links">
+              {footerLinks.map((elem) => (
+                <div className="footer-main__links-wrapper" key={elem.title}>
+                  <h4 className="footer-links__title">{elem.title}</h4>
+                  <ul className="footer-links__list">
+                    {elem.links.map((item) => (
+                      <li key={item.text}>
+                        <a href={item.link}>{item.text}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="footer-bottom"></div>
+          <div className="footer-bottom">
+            <div className="footer-bottom__social">
+              {footerSocial.map((elem) => (
+                <div className="footer-bootom__social-icon" key={elem}>
+                  <LazyImage
+                    src={elem}
+                    alt="social icon"
+                    className="footer-icon"
+                  />
+                </div>
+              ))}
+            </div>
+            <p className="footer-bottom__rights">
+              © {year} Ed-Circle. All rights reserved.
+            </p>
+          </div>
         </div>
       </div>
     </footer>
---
+++ my-app/src/components/Footer/footerdata.js
@@ -0,0 +1,28 @@
+import twitter from '/img/tvittter.svg';
+import linkedIn from '/img/linkedIn.svg';
+import gitHub from '/img/gitHub.svg';
+import facebook from '/img/facebook.svg';
+import social from '/img/social.svg';
+
+export const footerSocial = [twitter, linkedIn, gitHub, facebook, social];
+
+export const footerLinks = [
+  {
+    title: 'Company',
+    links: [
+      { text: 'About us', link: '/' },
+      { text: 'Courses', link: 'courses' },
+      { text: 'Reviews', link: 'reviews' },
+      { text: 'FAQ’s', link: 'faq' },
+    ],
+  },
+  {
+    title: 'Legal',
+    links: [
+      { text: 'Terms', link: 'terms' },
+      { text: 'Privacy', link: 'privacy' },
+      { text: 'Cookies', link: 'cookies' },
+      { text: 'Contact', link: 'contact' },
+    ],
+  },
+];
